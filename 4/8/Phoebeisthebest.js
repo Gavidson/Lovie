73efd0050f0fef50c0845e540c38e657b6631e71
Gavidson <144868879+Gavidson@users.noreply.github.com>
2023-09-20 16:20:54 -0400

    Update Phoebeisthebest.js

--- 4/8/Phoebeisthebest.js
+++ 4/8/Phoebeisthebest.js
@@ -1,25 +1,45 @@
-document.addEventListener("DOMContentLoaded", () => {
-  
-  const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const phrases = ["Hii Phoebe!! :)", "I Love You So So Much", "I think this is just a start to what I wanna make you.", "I miss you so much :(" , "Tell me what you think!", "Its gonna go back to the start but not all the way because idk how to do that yet!", "I love you kisses!", "Poop"];
+const totalRepetitions = 1;
+const h1Element = document.querySelector("h1");
+let currentPhraseIndex = 0;
+let repetitions = 0;
 
-  document.querySelector("h1").onmouseover = event => {
-    let iterations = 0;
+h1Element.addEventListener("mouseover",animateText);
+
+function animateText() {
+  let iterations = 0;
   
-    const interval = setInterval(() => {
-    event.target.innerText = event.target.innerText.split("")
-    .map((letter, index) => {
-      if(index < iterations) {
-        return event.target.dataset.value[index];
-      }
-    
-      return letters[Math.floor(Math.random() * 26)]
-    })
-    .join("");
-    
-    if(iterations >= event.target.dataset.value.length) {
-      clearInterval(interval);
+  const interval = setInterval(() => {
+  h1Element.innerText = phrases[currentPhraseIndex].split("")
+  .map((letter, index) => {
+    if(index < iterations) {
+      return phrases[currentPhraseIndex][index];
     }
-    iterations += 1 / 7;
-    }, 30);
+    
+    return letters[Math.floor(Math.random() * 26)]
+  })
+  .join("");
+    
+  if(iterations >= phrases[currentPhraseIndex].length) {
+    clearInterval(interval);
+    setTimeout(() => {
+        
+        if (currentPhraseIndex === 0) {
+            
+            if (repetitions === totalRepetitions) {
+              return; // Stop after the desired number of repetitions
+              
+            }
+            repetitions++;
+          }
+      
+        currentPhraseIndex = (currentPhraseIndex + 1) % phrases.length;
+        setTimeout(animateText, 1000); 
+      }, 1000);
+
+    
   }
-});
+  iterations += 1 / 3;
+  }, 30);
+}
